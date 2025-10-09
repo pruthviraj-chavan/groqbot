c65b41057c01b8c641fda3d85bdf5c0820fa62d4
Pruthviraj Chavan <91907487+pruthviraj-chavan@users.noreply.github.com>
2025-10-09 11:22:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import time
 
 app = Flask(__name__)
 
-# Minimal logging for speedddddd
+# Minimal logging for speed
 logging.basicConfig(level=logging.WARNING)  # Changed from INFO to WARNING
 logger = logging.getLogger(__name__)
 
